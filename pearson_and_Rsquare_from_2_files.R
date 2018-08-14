10d754e7869f4ab8921516f3362964c802a6fe84
lyraealpha <1036319848@qq.com>
2018-08-14 10:15:13 +0800

    none

---
+++ pearson_and_Rsquare_from_2_files.R
@@ -0,0 +1,22 @@
+#pearson_and_Rsquare_from_2_files
+
+fpkm1<-read.csv(file.choose(),header = T)
+fpkm2<-read.csv(file.choose(),header = T)
+matrix_a <-fpkm1
+matrix_b <-fpkm2Re
+
+
+r_square<-NULL
+pvalue <- NULL
+id<-(matrix_a[,1])
+for (i in 1:nrow(matrix_a)){
+  temp <- cor.test(as.numeric(matrix_a[i,]),as.numeric(matrix_b[i,]),method="pearson")
+  pvalue[i] <- temp$p.value
+  r_square[i]<-temp$estimate
+}
+Results <- data.frame(matrix_a[,1],r_square,pvalue)
+write.csv(Results,'C:/Users/vega/Documents/bioconductor-all/test.csv')
+
+
+
+#With the help of ZJ
